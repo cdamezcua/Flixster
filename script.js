f573d06a57e5ea65d72c0bb3f8621274c69f5ba9
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-06-15 01:19:50 -0700

    Implement movie search functionality

--- script.js
+++ script.js
@@ -7,10 +7,38 @@ var searchForm = document.getElementById("search-form");
 var searchInput = document.getElementById("search-input");
 var closeSearchButton = document.getElementById("close-search-btn");
 
-var searchMoviesGridContainer = document.getElementById("search-movies-grid-container");
+var searchMoviesGridContainer = document.getElementById(
+  "search-movies-grid-container"
+);
 var searchMoviesGrid = document.getElementById("search-movies-grid");
 
 var displayedPages = 1;
+
+function createMovieCard(movie) {
+  const divMovieCard = document.createElement("div");
+  divMovieCard.classList.add("movie-card");
+
+  const h1Title = document.createElement("h1");
+  h1Title.classList.add("movie-title");
+  h1Title.textContent = movie.title;
+
+  const imgPoster = document.createElement("img");
+  imgPoster.classList.add("movie-poster");
+  const imgPosterBaseURL = "https://image.tmdb.org/t/p/w500";
+  const imgPosterURL = imgPosterBaseURL + movie.poster_path;
+  imgPoster.setAttribute("src", imgPosterURL);
+  imgPoster.setAttribute("alt", movie.title + " poster.");
+
+  const pVoteAverage = document.createElement("p");
+  pVoteAverage.classList.add("movie-votes");
+  pVoteAverage.textContent = "⭐️ " + movie.vote_average;
+
+  divMovieCard.appendChild(h1Title);
+  divMovieCard.appendChild(imgPoster);
+  divMovieCard.appendChild(pVoteAverage);
+
+  return divMovieCard;
+}
 
 async function loadNowPlayingMoviesPage(page) {
   try {
@@ -28,29 +56,7 @@ async function loadNowPlayingMoviesPage(page) {
 
     const movies = data.results;
     movies.forEach((movie) => {
-      const divMovieCard = document.createElement("div");
-      divMovieCard.classList.add("movie-card");
-
-      const h1Title = document.createElement("h1");
-      h1Title.classList.add("movie-title");
-      h1Title.textContent = movie.title;
-
-      const imgPoster = document.createElement("img");
-      imgPoster.classList.add("movie-poster");
-      const imgPosterBaseURL = "https://image.tmdb.org/t/p/w500";
-      const imgPosterURL = imgPosterBaseURL + movie.poster_path;
-      imgPoster.setAttribute("src", imgPosterURL);
-      imgPoster.setAttribute("alt", movie.title + " poster.");
-
-      const pVoteAverage = document.createElement("p");
-      pVoteAverage.classList.add("movie-votes");
-      pVoteAverage.textContent = "⭐️ " + movie.vote_average;
-
-      divMovieCard.appendChild(h1Title);
-      divMovieCard.appendChild(imgPoster);
-      divMovieCard.appendChild(pVoteAverage);
-
-      moviesGrid.appendChild(divMovieCard);
+      moviesGrid.appendChild(createMovieCard(movie));
     });
   } catch (error) {
     console.log(error);
@@ -71,4 +77,40 @@ searchButton.addEventListener("click", (event) => {
   MoviesGridContainer.style.display = "none";
   searchForm.style.display = "block";
   searchMoviesGridContainer.style.display = "block";
+});
+
+closeSearchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  searchForm.style.display = "none";
+  searchMoviesGridContainer.style.display = "none";
+  searchButton.style.display = "block";
+  MoviesGridContainer.style.display = "block";
+});
+
+searchInput.addEventListener("keydown", async (event) => {
+  console.log("Key pressed.");
+  if (event.key === "Enter") {
+    event.preventDefault();
+    console.log("Enter key pressed.");
+    searchMoviesGrid.innerHTML = "";
+    try {
+      const baseURL = "https://api.themoviedb.org/3/search/movie";
+      const apikey = "b55c170bfdfea258df28f3ba96c063b4";
+      const query = searchInput.value;
+
+      const url = new URL(baseURL);
+      url.searchParams.append("api_key", apikey);
+      url.searchParams.append("query", query);
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      const movies = data.results;
+      movies.forEach((movie) => {
+        searchMoviesGrid.appendChild(createMovieCard(movie));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 });
